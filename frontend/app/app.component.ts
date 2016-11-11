00f970adcefcd6bcdaf85e9318190b39f572e85f
Fernando Costa <fadc80@gmail.com>
2016-11-11 06:09:56 -0300

    Refactoring template e CSS content

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -4,68 +4,10 @@ import { Contact }   from './contact';
 import { ContactListService } from './contact-list.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <h2><b>{{subtitle}}</b></h2>
-    <ul class="contacts">
-      <li *ngFor="let contact of contacts"
-        [class.selected]="contact === selectedContact"
-        (click)="onSelect(contact)">
-        <span class="badge">{{contact.id}}</span> 
-          {{contact.firstName}} {{contact.lastName}}
-      </li>
-    </ul>
-  `,
-  styles: [`
-    .selected {
-      background-color: #CFD8DC !important;
-      color: white;
-    }
-    .contacts {
-      margin: 0 0 2em 0;
-      list-style-type: none;
-      padding: 0;
-      width: 15em;
-    }
-    .contacts li {
-      cursor: pointer;
-      position: relative;
-      left: 0;
-      background-color: #EEE;
-      margin: .4em;
-      padding: .3em 0;
-      height: 1.6em;
-      border-radius: 4px;
-    }
-    .contacts li.selected:hover {
-      background-color: #BBD8DC !important;
-      color: white;
-    }
-    .contacts li:hover {
-      color: #607D8B;
-      background-color: #DDD;
-      left: .1em;
-    }
-    .heroes .text {
-      position: relative;
-      top: -3px;
-    }
-    .contacts .badge {
-      display: inline-block;
-      font-size: small;
-      color: white;
-      padding: 0.8em 0.7em 0 0.7em;
-      background-color: #607D8B;
-      line-height: 1em;
-      position: relative;
-      left: -1px;
-      top: -4px;
-      height: 1.8em;
-      margin-right: .8em;
-      border-radius: 4px 0 0 4px;
-    }
-  `]
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 
 export class AppComponent implements OnInit { 
